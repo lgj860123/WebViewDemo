9b90e63aa743f022cf9b59c88a84bef974d0fbdf
lgj860123 <304035749@qq.com>
2018-04-07 22:32:13 +0800

    测试X5内核

---
+++ app/src/main/java/com/example/luogj/webviewdemo/interfaces/WebViewJavaScriptFunction.java
@@ -0,0 +1,9 @@
+package com.example.luogj.webviewdemo.interfaces;
+
+/**
+ * Created by Administrator on 2018/4/7.
+ */
+
+public interface WebViewJavaScriptFunction {
+    void onJsFunctionCalled(String tag);
+}
